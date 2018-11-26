59c928bbf384f4a886f7c31182be3cc1a06f7d8c
Soni <soni.kumari@interactive12.com>
2018-11-26 18:46:34 +0530

    adding city controller

---
+++ Admin/Models/cityModal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Admin.Models
+{
+    public class cityModal
+    {
+       
+            ///<summary>
+            /// Gets or sets Customers.
+            ///</summary>
+            public List<Admin.Modal.SodCityCodeMaster> City { get; set; }
+
+            ///<summary>
+            /// Gets or sets CurrentPageIndex.
+            ///</summary>
+            public int CurrentPageIndex { get; set; }
+
+            ///<summary>
+            /// Gets or sets PageCount.
+            ///</summary>
+            public int PageCount { get; set; }
+        
+    }
+}
